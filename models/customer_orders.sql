ed1de16ac99a6023599ce2479c4ed8953c8643a3
andrii.kobyakovskiy@gmail.com <andrii.kobyakovskiy@gmail.com>
2025-03-30 20:23:31 +0000

    Implemented staging models

--- models/customer_orders.sql
+++ models/customer_orders.sql
@@ -1,42 +1,46 @@
-WITH paid_orders as (select Orders.ID as order_id,
-    Orders.USER_ID	as customer_id,
-    Orders.ORDER_DATE AS order_placed_at,
-        Orders.STATUS AS order_status,
-    p.total_amount_paid,
-    p.payment_finalized_date,
-    C.FIRST_NAME    as customer_first_name,
-        C.LAST_NAME as customer_last_name
-FROM jaffle_shop.orders as Orders
-left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from stripe.payment
-        where STATUS <> 'fail'
-        group by 1) p ON orders.ID = p.order_id
-left join jaffle_shop.customers C on orders.USER_ID = C.ID ),
+WITH paid_orders as (
+    select 
+        orders.order_id as order_id
+        , Orders.USER_ID	as customer_id
+        , Orders.ORDER_DATE AS order_placed_at
+        , Orders.STATUS AS order_status
+        , p.total_amount_paid
+        , p.payment_finalized_date
+        , C.FIRST_NAME as customer_first_name
+        , C.LAST_NAME as customer_last_name
+    FROM {{ ref('orders') }} as Orders
+    left join (select order_id, max(created_at) as payment_finalized_date, sum(amount) / 100.0 as total_amount_paid
+            from {{ ref('payments') }}
+            where STATUS <> 'fail'
+            group by 1) p ON orders.order_id = p.order_id
+    left join {{ ref('customers') }} C on orders.USER_ID = C.customer_id ),
 
-customer_orders 
-as (select C.ID as customer_id
-    , min(ORDER_DATE) as first_order_date
-    , max(ORDER_DATE) as most_recent_order_date
-    , count(ORDERS.ID) AS number_of_orders
-from jaffle_shop.customers C 
-left join jaffle_shop.orders as Orders
-on orders.USER_ID = C.ID 
-group by 1)
+customer_orders as (
+    select 
+        C.customer_id as customer_id
+        , min(ORDER_DATE) as first_order_date
+        , max(ORDER_DATE) as most_recent_order_date
+        , count(orders.order_id) AS number_of_orders
+    from {{ ref('customers') }} C 
+    left join {{ ref('orders') }} as Orders
+    on orders.USER_ID = C.customer_id 
+    group by 1
+)
 
 select
-p.*,
-ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-CASE WHEN c.first_order_date = p.order_placed_at
-THEN 'new'
-ELSE 'return' END as nvsr,
-x.clv_bad as customer_lifetime_value,
-c.first_order_date as fdos
+    p.*,
+    ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
+    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
+    CASE WHEN c.first_order_date = p.order_placed_at
+    THEN 'new'
+    ELSE 'return' END as nvsr,
+    x.clv_bad as customer_lifetime_value,
+    c.first_order_date as fdos
 FROM paid_orders p
 left join customer_orders as c USING (customer_id)
 LEFT OUTER JOIN 
 (
-        select
+    select
         p.order_id,
         sum(t2.total_amount_paid) as clv_bad
     from paid_orders p
